0f2d81cf31ec17f33802ba0116fcfb1622d002ae
Dibyajyoti <dmishra0323@gmail.com>
2022-04-11 09:43:27 +0530

    make it better

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 
 # importing the necessary dependencies
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+
 import pickle
+
+from flask import Flask, render_template, request
+from flask_cors import CORS,cross_origin
 
 app = Flask(__name__) # initializing a flask app
 
